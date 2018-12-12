042b7257353111d3ed45eb45f90a2815d9508979
Maxim Oancha <maksimoancha12@gmail.com>
2018-12-12 13:53:46 +0200

    refactor comment parser

---
+++ database/schemas/PostSchema.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    author: {type: String, index: true},
+    permlink: {type: String, index: true},
+    parent_author: {type: String, required: true, default: ''},
+    parent_permlink: {type: String, required: true},
+    title: {type: String, required: true, default: ''},
+    body: {type: String, required: true, default: ''},
+    json_metadata: {type: String, required: true, default: ''},
+    depth: {type: Number, default: 0},
+    total_vote_weight: {type: Number, default: 0},
+    active_votes: [{
+        voter: {type: String, required: true},
+        author: {type: String, required: true},
+        permlink: {type: String, required: true},
+        weight: {type: Number, required: true}
+    }],
+    wobjects:[{
+        author_permlink: {type: String, index: true},
+        percent: {type: Number}
+    }]
+}, {timestamps: true});
+
+PostSchema.index({author: 1, permlink: 1},{unique: true});
+
+const PostModel = mongoose.model('Post', PostSchema);
+
+module.exports = PostModel;
