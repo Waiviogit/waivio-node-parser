4eb7cec99ac16906ec106016c230c036a46f12a0
Maxim Oancha <maksimoancha12@gmail.com>
2019-03-28 16:54:39 +0200

    fix

--- utilities/helpers/investarenaForecastHelper.js
+++ utilities/helpers/investarenaForecastHelper.js
@@ -3,7 +3,7 @@ const {postsUtil} = require('../steemApi');
 const SUPPORTED_EXP_FORECAST_BOTS = 'z1wo5,et42k,suy38,x45ki,q13lp,j5gs7,q1w2c,no58s,mhg41,b23df,vp4g5,an98r,npo31,w1c6c,nn13b'.split(',');
 
 const updatePostWithForecast = async ({author, permlink, forecast}) => {
-    const existing = await Post.findOne({author: data.author, permlink: data.permlink});
+    const existing = await Post.findOne({author: author, permlink: permlink});
     let post = {author, permlink};
     if (!existing.post) {
         let {post: steemPost, err} = await postsUtil.getPost(author, permlink);
@@ -22,14 +22,13 @@ const updatePostWithForecast = async ({author, permlink, forecast}) => {
 };
 
 const updatePostWithExpForecast = async ({parent_author, parent_permlink, author, exp_forecast}) => {
-    const isValid = await validateExpForecast({parent_permlink, parent_author, author});
+    const isValid = await validateExpForecast({parent_permlink, parent_author, author, exp_forecast});
     if (isValid) {
         const {result, error} = await Post.update({author: parent_author, permlink: parent_permlink, exp_forecast});
         if (result)
             return true;
     }
     return false;
-
 };
 
 const validateExpForecast = async ({parent_author, parent_permlink, author, exp_forecast}) => {
