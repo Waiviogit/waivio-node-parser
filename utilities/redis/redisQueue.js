5f6ecfb1332e4dc6223e788a0311fc7c5b50fed4
Maxim <maksimoancha12@gmail.com>
2019-04-04 17:39:10 +0300

    add validators

--- utilities/redis/redisQueue.js
+++ utilities/redis/redisQueue.js
@@ -2,13 +2,17 @@ const RedisSMQ = require("rsmq");
 const rsmq = new RedisSMQ({ns: 'rsmq', realtime: true, options: {db: 10}});
 
 const createQueue = async () => {
-    rsmq.createQueueAsync({qname: "object_queue"}).then(function (resp) {
-        if (resp === 1) {
-            console.log("queue 'object_queue' created")
+    try {
+        const res = await rsmq.createQueueAsync({qname: "object_queue"});
+        if (res === 1) {
+            return {result: true};
+        } else {
+            result: false
         }
-    });
-};
-
+    } catch (e) {
+        return {error: e}
+    }
+}
 const sendMessage = async () => {
     const la = JSON.stringify({
         type: 'objectType',
@@ -23,14 +27,14 @@ const sendMessage = async () => {
 const receiveMessage = async () => {
     const resp = await rsmq.receiveMessageAsync({qname: "object_queue"});
     if (resp.id) {
-        await new Promise(f => setTimeout(f, 400));
+        // await new Promise(f => setTimeout(f, 400));
         console.log("Message received.", resp);
         const mess = JSON.parse(resp.message);
-        console.log('TYPE:  ', mess.type);
-        await new Promise(f => setTimeout(f, 1500));
+        console.log('TYPE:  ', mess ? mess.type : null);
+        // await new Promise(f => setTimeout(f, 1500));
         const delResp = await rsmq.deleteMessageAsync({qname: 'object_queue', id: resp.id});
         if (delResp === 1) {
-            await new Promise(f => setTimeout(f, 900));
+            await new Promise(f => setTimeout(f, 500));
             console.log(`Message: "${resp.message}" deleted!`);
             return true;
         }
@@ -40,24 +44,25 @@ const receiveMessage = async () => {
     }
 };
 
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-// createQueue();
 (async () => {
-    for(let i = 0; i<5 ; i++){
+    const {result, error} = await createQueue();
+    if (error) {
+        console.error(error.message);
+    }
+    for (let i = 0; i < 5; i++) {
         await sendMessage();
     }
     let delMessage = true;
-    while(delMessage){
+    let counter = 0;
+    while (delMessage) {
         delMessage = await receiveMessage();
-        if(!delMessage){
-            await new Promise(f => setTimeout(f, 3000));
+        if (!delMessage) {
+            await new Promise(f => setTimeout(f, 100));
             delMessage = true;
         }
+        if(counter === 5000){
+            await sendMessage();
+        }
+        counter++;
     }
-})();
-
-
-
+})()
