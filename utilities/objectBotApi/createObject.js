e6a2a9098d3dab34ca99d9b8aa0e25b24565305d
Maxim <maksimoancha12@gmail.com>
2019-04-05 13:14:47 +0300

    add test for objectBotApi services

--- utilities/objectBotApi/createObject.js
+++ utilities/objectBotApi/createObject.js
@@ -1,7 +1,26 @@
 const axios = require('axios');
+const {OBJECT_BOT_HOST_URL, CREATE_OBJECT_ROUTE} = require('../../constants/appData').objectsBot;
 const {createObjectValidate} = require('./validators');
+const URL = OBJECT_BOT_HOST_URL + CREATE_OBJECT_ROUTE;
 
-const send = (data) => {
-    
+const send = async (data) => {
+    const {error} = createObjectValidate(data);
+    if (error) {
+        console.error(error);
+        return {error};
+    }
+    try {
+        const {data: response} = await axios.post(URL, data);
+        if (response && response.transactionId && response.parentAuthor && response.parentPermlink && response.author && response.permlink) {
+            return response;
+        } else {
+            return {error:{message: 'Not enough response data!'}}
+        }
+    } catch (err) {
+        console.error(err);
+        return {error: err};
+    }
 };
+
+module.exports = {send};
 
--- utilities/objectBotApi/validators.js
+++ utilities/objectBotApi/validators.js
@@ -5,37 +5,40 @@ const CREATE_OBJECT_REQUIRED_FIELDS = 'author,title,body,permlink,objectName,loc
 const APPEND_OBJECT_REQUIRED_FIELD = 'author,permlink,parentAuthor,parentPermlink,body,title,field'.split(',');
 
 const createObjectTypeValidate = (data) => {
-    for (const field in CREATE_OBJECT_TYPE_REQUIRED_FIELDS) {
+    for (const field of CREATE_OBJECT_TYPE_REQUIRED_FIELDS) {
         if (_.isNil(data[field])) {
             return {error: {message: `Create Object Type data is not valid, ${field} field not found!`}}
         }
     }
+    return {isValid: true}
 };
 
 const createObjectValidate = (data) => {
-    for (const field in CREATE_OBJECT_REQUIRED_FIELDS) {
+    for (const field of CREATE_OBJECT_REQUIRED_FIELDS) {
         if (_.isNil(data[field])) {
             return {error: {message: `Create Object data is not valid, ${field} field not found!`}}
         }
     }
+    return {isValid: true}
 };
 
 const appendObjectValidate = (data) => {
-    for (const field in APPEND_OBJECT_REQUIRED_FIELD) {
+    for (const field of APPEND_OBJECT_REQUIRED_FIELD) {
         if (_.isNil(data[field])) {
             return {error: {message: `Append Object data is not valid, ${field} field not found!`}}
         }
     }
     const REQUIRED_FIELD_FIELDS = 'name,body,locale'.split(',');
-    for (const field in REQUIRED_FIELD_FIELDS) {
+    for (const field of REQUIRED_FIELD_FIELDS) {
         if (_.isNil(data.field[field])) {
             return {error: {message: `Append Object data is not valid, field.${field} field not found!`}}
         }
     }
+    return {isValid: true}
 };
 
 module.exports = {
     createObjectTypeValidate,
     createObjectValidate,
     appendObjectValidate
-}
+};
