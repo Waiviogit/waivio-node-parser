5c0d195dcbad53615441f69ac6037bbb349fd25f
Maxim Oancha <maksimoancha12@gmail.com>
2019-06-19 14:35:40 +0300

    add staging env

--- parsers/mainParser.js
+++ parsers/mainParser.js
@@ -1,5 +1,6 @@
 const { followObjectParser, commentParser, voteParser, userParsers } = require( '../parsers' );
 const { ratingHelper } = require( '../utilities/helpers' );
+const PARSE_ONLY_VOTES = process.env.PARSE_ONLY_VOTES === 'true';
 
 const parseSwitcher = async ( transactions ) => {
     const votesOps = [];
@@ -7,34 +8,41 @@ const parseSwitcher = async ( transactions ) => {
     for ( const transaction of transactions ) {
         if ( transaction && transaction.operations && transaction.operations[ 0 ] ) {
             for ( const operation of transaction.operations ) {
-                switch ( operation[ 0 ] ) {
-                    case 'comment' :
-                        await commentParser.parse( operation[ 1 ] );
-                        break;
-                    case 'vote' :
-                        votesOps.push( operation[ 1 ] );
-                        break;
-                    case 'custom_json' :
-                        switch ( operation[ 1 ].id ) {
-                            case 'follow_wobject' :
-                                await followObjectParser.parse( operation[ 1 ] );
-                                break;
-                            case 'wobj_rating' :
-                                await ratingHelper.parse( operation[ 1 ] );
-                                break;
+                if( !PARSE_ONLY_VOTES ) {
+                    switch ( operation[ 0 ] ) {
+                        case 'comment' :
+                            await commentParser.parse( operation[ 1 ] );
+                            break;
+                        case 'custom_json' :
+                            switch ( operation[ 1 ].id ) {
+                                case 'follow_wobject' :
+                                    await followObjectParser.parse( operation[ 1 ] );
+                                    break;
+                                case 'wobj_rating' :
+                                    await ratingHelper.parse( operation[ 1 ] );
+                                    break;
 
-                            case 'follow' :
-                                await userParsers.followUserParser( operation[ 1 ] );
-                                break;
-                        }
-                        break;
-                    case 'account_update' :
-                        await userParsers.updateAccountParser( operation[ 1 ] );
+                                case 'follow' :
+                                    await userParsers.followUserParser( operation[ 1 ] );
+                                    break;
+                            }
+                            break;
+                        case 'account_update' :
+                            await userParsers.updateAccountParser( operation[ 1 ] );
+                            break;
+                        case 'vote' :
+                            votesOps.push( operation[ 1 ] );
+                            break;
+                    }
+                } else if( operation[ 0 ] === 'vote' ) {
+                    votesOps.push( operation[ 1 ] );
                 }
             }
         }
     }
-    await voteParser.parse( votesOps );
+    if( PARSE_ONLY_VOTES ) {
+        await voteParser.parse( votesOps );
+    }
 };
 
 module.exports = {
