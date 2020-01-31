b44a8b872275123dd25875ddf187e8930fa8799d
olegtsigulov <olegcigulev@gmail.com>
2020-01-31 15:27:07 +0200

    refactor

--- utilities/helpers/updatePostAfterComment.js
+++ utilities/helpers/updatePostAfterComment.js
@@ -8,7 +8,7 @@ const updateCounters = async( author, permlink ) => {
         console.error( err.message );
         return;
     }
-    if( post ) {
+    if( post.author ) {
         let metadata = {};
         try{
             metadata = JSON.parse( post.json_metadata );
