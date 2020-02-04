05e8139ab3a22a9776a95f6021a624a557b0a293
Maksim <maksimoancha12@gmail.com>
2020-02-04 13:37:18 +0200

    edit eslint config, refactor validators with new eslint rules

--- validator/wobjectValidator.js
+++ validator/wobjectValidator.js
@@ -1,30 +1,30 @@
-const { validateUserOnBlacklist } = require( './userValidator' );
-const _ = require( 'lodash' );
+const _ = require('lodash');
+const { validateUserOnBlacklist } = require('./userValidator');
 
-const validateRatingVote = ( data, operation ) => {
-    if( !validateUserOnBlacklist( _.get( operation, 'required_posting_auths[0]' ) ) ) return false;
-    const requiredFieldsRatingVote = 'author,permlink,author_permlink,rate'.split( ',' );
+const validateRatingVote = (data, operation) => {
+  if (!validateUserOnBlacklist(_.get(operation, 'required_posting_auths[0]'))) return false;
+  const requiredFieldsRatingVote = 'author,permlink,author_permlink,rate'.split(',');
 
-    for( let i = 0; i < requiredFieldsRatingVote.length; i++ ) {
-        if ( _.isNil( data[ requiredFieldsRatingVote[ i ] ] ) ) {
-            return false;
-        }
+  for (let i = 0; i < requiredFieldsRatingVote.length; i++) {
+    if (_.isNil(data[requiredFieldsRatingVote[i]])) {
+      return false;
     }
+  }
 
-    return _.get( data, 'rate' ) <= 10 && _.get( data, 'rate' ) >= 0;
+  return _.get(data, 'rate') <= 10 && _.get(data, 'rate') >= 0;
 };
 
-const validateObjectType = ( data ) => {
-    const requiredFieldsObjectType = 'author,permlink,name'.split( ',' );
-    for ( let i = 0; i < requiredFieldsObjectType.length;i++ ) {
-        if( _.isNil( data[ requiredFieldsObjectType[ i ] ] ) ) {
-            return false;
-        }
+const validateObjectType = (data) => {
+  const requiredFieldsObjectType = 'author,permlink,name'.split(',');
+  for (let i = 0; i < requiredFieldsObjectType.length; i++) {
+    if (_.isNil(data[requiredFieldsObjectType[i]])) {
+      return false;
     }
+  }
 
-    return validateUserOnBlacklist( _.get( data, 'author' ) );
+  return validateUserOnBlacklist(_.get(data, 'author'));
 };
 
 module.exports = {
-    validateRatingVote, validateObjectType
+  validateRatingVote, validateObjectType,
 };
