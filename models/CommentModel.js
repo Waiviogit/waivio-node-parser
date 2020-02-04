7abb41edfb2614a00ef5c073f3d44060632e8240
Maksim <maksimoancha12@gmail.com>
2020-02-04 18:23:41 +0200

    refactor models with new eslint rules

--- models/CommentModel.js
+++ models/CommentModel.js
@@ -1,38 +1,41 @@
-const { Comment } = require( '../database' ).models;
-const UserModel = require( './UserModel' );
-const _ = require( 'lodash' );
+const _ = require('lodash');
+const { Comment } = require('../database').models;
+const UserModel = require('./UserModel');
 
-exports.createOrUpdate = async function ( comment ) {
-    await UserModel.checkAndCreate( comment.author ); // create user in DB if it doesn't exist
-    try {
-        const newComment = await Comment.findOneAndUpdate(
-            { ..._.pick( comment, [ 'author', 'permlink' ] ) },
-            { ...comment },
-            { setDefaultsOnInsert: true, upsert: true, new: true } );
-        return { comment: newComment.toObject() };
-    } catch ( error ) {
-        return { error };
-    }
+exports.createOrUpdate = async (comment) => {
+  await UserModel.checkAndCreate(comment.author); // create user in DB if it doesn't exist
+  try {
+    const newComment = await Comment.findOneAndUpdate(
+      { ..._.pick(comment, ['author', 'permlink']) },
+      { ...comment },
+      { setDefaultsOnInsert: true, upsert: true, new: true },
+    );
+    return { comment: newComment.toObject() };
+  } catch (error) {
+    return { error };
+  }
 };
 
-exports.getOne = async ( { author, permlink } ) => {
-    try {
-        return { comment: await Comment.findOne( { author, permlink } ).lean() };
-    } catch ( error ) {
-        return { error };
-    }
+exports.getOne = async ({ author, permlink }) => {
+  try {
+    return { comment: await Comment.findOne({ author, permlink }).lean() };
+  } catch (error) {
+    return { error };
+  }
 };
 
-exports.addVote = async ( { author, permlink, voter, percent } ) => {
-    try {
-        await Comment.updateOne( { author, permlink }, { $pull: { active_votes: { voter } } } );
-        return {
-            result: await Comment.updateOne(
-                { author, permlink },
-                { $addToSet: { active_votes: { voter, percent } } }
-            )
-        };
-    } catch ( error ) {
-        return { error };
-    }
+exports.addVote = async ({
+  author, permlink, voter, percent,
+}) => {
+  try {
+    await Comment.updateOne({ author, permlink }, { $pull: { active_votes: { voter } } });
+    return {
+      result: await Comment.updateOne(
+        { author, permlink },
+        { $addToSet: { active_votes: { voter, percent } } },
+      ),
+    };
+  } catch (error) {
+    return { error };
+  }
 };
