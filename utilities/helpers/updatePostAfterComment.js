18d96caf7360d2b9816bd23b4b1f2f7cf0893480
Maksim <maksimoancha12@gmail.com>
2020-02-06 12:41:27 +0200

    fix updatePostAfterComment

--- utilities/helpers/updatePostAfterComment.js
+++ utilities/helpers/updatePostAfterComment.js
@@ -1,12 +1,28 @@
 const { postsUtil } = require('utilities/steemApi');
 const { Post } = require('database').models;
 
+/**
+ * Update post "children" counter by comment "author" and "permlink"
+ * @param author Author of comment on post
+ * @param permlink Permlink of comment on post
+ * @returns {Promise<void>}
+ */
 const updateCounters = async (author, permlink) => {
-  const { post, err } = await postsUtil.getPost(author, permlink);
+  const { post: comment, err } = await postsUtil.getPost(author, permlink);
   if (err) {
     console.error(err && err.message ? err.message : err);
     return;
   }
+  if (!comment || !comment.author) {
+    return console.error(`[Update Post counters] Comment ${author}/${permlink} not exist or was deleted!`);
+  }
+
+  const { post, err: error } = await postsUtil.getPost(comment.root_author, comment.root_permlink);
+  if (error) {
+    console.error(error && error.message ? error.message : error);
+    return;
+  }
+
   if (post && post.author) {
     try {
       const res = await Post.updateOne({
@@ -15,8 +31,8 @@ const updateCounters = async (author, permlink) => {
         children: post.children,
       });
       if (res.ok) console.log(`Post @${author}/${permlink} updated!`);
-    } catch (error) {
-      console.error(error);
+    } catch (e) {
+      console.error(e);
     }
   }
 };
