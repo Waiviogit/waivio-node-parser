6bf1ec5094acad4ea7bef6475c14ef7322b8c3fd
olegtsigulov <olegcigulev@gmail.com>
2020-03-24 14:14:44 +0200

    immediately import

--- utilities/objectImportServiceApi/importUpdates.js
+++ utilities/objectImportServiceApi/importUpdates.js
@@ -6,7 +6,8 @@ const URL = IMPORT_OBJECTS_SERVICE_HOST_URL + IMPORT_UPDATES_ROUTE;
 const send = async (wobjects) => {
   if (wobjects && Array.isArray(wobjects) && wobjects.length) {
     try {
-      const { data: response } = await axios.post(URL, { wobjects });
+      const { data: response } = await axios.post(URL,
+        { wobjects, immediately: true }, { headers: { API_KEY: process.env.API_KEY } });
 
       if (response) {
         return { response };
