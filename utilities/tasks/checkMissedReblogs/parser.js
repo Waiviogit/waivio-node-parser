b9aa785661b06ec1945b7d142deb6ad55717e107
olegtsigulov <olegcigulev@gmail.com>
2020-07-16 11:46:43 +0300

    refactor reblog task

--- utilities/tasks/checkMissedReblogs/parser.js
+++ utilities/tasks/checkMissedReblogs/parser.js
@@ -7,6 +7,16 @@ const reblogParseSwitcher = async (transactions) => {
   for (const transaction of transactions) {
     if (transaction && transaction.operations && transaction.operations[0]) {
       for (const operation of transaction.operations) {
+        if (operation[0] === 'comment') {
+          if (operation[1].parent_author) continue;
+          const { post } = await Post.findOne(operation[1]);
+          if (!post) {
+            await postHelper.createPost({
+              author: operation[1].author, permlink: operation[1].permlink, commentParser, fromTTL: true,
+            });
+          }
+        }
+
         if (operation[0] === 'custom_json' && operation[1].id === 'follow') {
           await reblogParser(operation[1], transaction.block_num, transaction.expiration);
         }
