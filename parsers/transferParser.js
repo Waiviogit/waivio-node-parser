0caec741366bce192a74edaf9dba1e1a7c2d569e
Maxim <maksimoancha12@gmail.com>
2020-07-17 17:04:27 +0300

    add new collection wallets

--- parsers/transferParser.js
+++ parsers/transferParser.js
@@ -1,7 +1,7 @@
 const notificationsUtil = require('utilities/notificationsApi/notificationsUtil');
 const { walletHelper } = require('utilities/helpers');
 
-const parse = async (operation) => {
+const parse = async (operation, trxId) => {
   await notificationsUtil.custom(Object.assign(operation, { id: 'transfer' }));
   const data = {
     type: 'transfer',
@@ -9,22 +9,24 @@ const parse = async (operation) => {
     to: operation.to,
     amount: operation.amount,
     memo: operation.memo,
+    trxId,
   };
   await walletHelper.addToWallet(data);
 };
 
-const parseVesting = async (operation) => {
+const parseVesting = async (operation, trxId) => {
   await notificationsUtil.custom(Object.assign(operation, { id: 'transfer_to_vesting' }));
   const data = {
     type: 'transfer_to_vesting',
     account: operation.from,
     to: operation.to,
     amount: operation.amount,
+    trxId,
   };
   await walletHelper.addToWallet(data);
 };
 
-const parseFromSavings = async (operation) => {
+const parseFromSavings = async (operation, trxId) => {
   await notificationsUtil.custom(Object.assign(operation, { id: 'transfer_from_savings' }));
   const data = {
     type: 'transfer_from_savings',
@@ -32,16 +34,18 @@ const parseFromSavings = async (operation) => {
     to: operation.to,
     memo: operation.memo,
     request_id: operation.request_id,
+    trxId,
   };
   await walletHelper.addToWallet(data);
 };
 
-const parseToSavings = async (operation) => {
+const parseToSavings = async (operation, trxId) => {
   const data = {
     type: 'transfer_to_savings',
     account: operation.from,
     to: operation.to,
     memo: operation.memo,
+    trxId,
   };
   await walletHelper.addToWallet(data);
 };
