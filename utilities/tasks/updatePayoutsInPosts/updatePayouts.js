e7f94b594479e2393fb7dde08a383076025c6ac9
Boris Andrzhiievskyi <borees2@gmail.com>
2020-07-17 13:04:55 +0300

    add lean()

--- utilities/tasks/updatePayoutsInPosts/updatePayouts.js
+++ utilities/tasks/updatePayoutsInPosts/updatePayouts.js
@@ -9,14 +9,14 @@ const updatePayouts = async () => {
 
   await co(function *iterateCursor() {
     const cursor = Post
-      .find({ createdAt: { $lte: new Date(sevenDaysAgo) } }).cursor();
+      .find({ createdAt: { $lte: new Date(sevenDaysAgo) } }).lean().cursor();
     for (let doc = yield cursor.next(); doc != null; doc = yield cursor.next()) {
-      const author = _.get(doc, '_doc.author', 0);
-      const permlink = _.get(doc, '_doc.permlink', 0);
-      const cashoutTime = _.get(doc, '_doc.cashout_time', 0);
-      const totalPayoutValue = _.get(doc, '_doc.total_payout_value', 0);
-      const pendingPayoutValue = _.get(doc, '_doc.pending_payout_value', 0);
-      const curatorPayoutValue = _.get(doc, '_doc.curator_payout_value', 0);
+      const author = _.get(doc, 'author', 0);
+      const permlink = _.get(doc, 'permlink', 0);
+      const cashoutTime = _.get(doc, 'cashout_time', 0);
+      const totalPayoutValue = _.get(doc, 'total_payout_value', 0);
+      const pendingPayoutValue = _.get(doc, 'pending_payout_value', 0);
+      const curatorPayoutValue = _.get(doc, 'curator_payout_value', 0);
 
       const expiredCashOut = new Date(cashoutTime).valueOf() + 1800000 < new Date().valueOf()
           && parseFloat(pendingPayoutValue) > 0;
