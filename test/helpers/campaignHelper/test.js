8abee82671584f95abee9506e19efd9bdd024e5d
Boris Andrzhiievskyi <borees2@gmail.com>
2020-08-12 13:53:06 +0300

    correct path to test helper and Factory

--- test/helpers/campaignHelper/test.js
+++ test/helpers/campaignHelper/test.js
@@ -1,7 +1,7 @@
 const {
   expect, campaignHelper, dropDatabase, faker, CampaignModel,
-} = require('../../testHelper');
-const { CampaignFactory } = require('../../factories');
+} = require('test/testHelper');
+const { CampaignFactory } = require('test/factories');
 
 describe('Campaign Helper', async () => {
   describe('parseReservationConversation', async () => {
@@ -15,7 +15,7 @@ describe('Campaign Helper', async () => {
         operation = { author: faker.name.firstName(), parent_permlink: faker.name.firstName() };
         returnedValue = await campaignHelper.parseReservationConversation(operation);
       });
-      it('should return true when campaign not found ', async () => {
+      it('parseReservationConversation should return true when campaign not found ', async () => {
         expect(returnedValue).to.be.true;
       });
       it('should not find company', async () => {
@@ -29,7 +29,7 @@ describe('Campaign Helper', async () => {
         operation = { author: faker.name.firstName(), parent_permlink: campaign.users[0].permlink };
         returnedValue = await campaignHelper.parseReservationConversation(operation);
       });
-      it('should return true', async () => {
+      it('parseReservationConversation should return true', async () => {
         expect(returnedValue).to.be.true;
       });
       it('should increment children to 1', async () => {
@@ -43,7 +43,7 @@ describe('Campaign Helper', async () => {
         operation = { author: campaign.users[0].name, parent_permlink: campaign.users[0].permlink };
         returnedValue = await campaignHelper.parseReservationConversation(operation);
       });
-      it('should return false', async () => {
+      it('parseReservationConversation should return false', async () => {
         expect(returnedValue).to.be.false;
       });
       it('should increment children to 1', async () => {
