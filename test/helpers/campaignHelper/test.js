2fcec3ee70fa1a96c7874b9b749e59bb4e32671c
Boris Andrzhiievskyi <borees2@gmail.com>
2020-09-18 17:22:58 +0300

    fix tests

--- test/helpers/campaignHelper/test.js
+++ test/helpers/campaignHelper/test.js
@@ -1,7 +1,7 @@
 const {
-  expect, campaignHelper, dropDatabase, faker, CampaignModel, config,
+  expect, campaignHelper, dropDatabase, faker, CampaignModel,
 } = require('test/testHelper');
-const { CampaignFactory, AppFactory } = require('test/factories');
+const { CampaignFactory } = require('test/factories');
 
 describe('Campaign Helper', async () => {
   describe('parseReservationConversation', async () => {
@@ -24,18 +24,13 @@ describe('Campaign Helper', async () => {
         expect(result).to.be.null;
       });
     });
-    describe('when function found campaign but user did not reserve it', async () => {
+    describe('when function  user did not reserve campaign', async () => {
       beforeEach(async () => {
-        operation = { author: faker.name.firstName(), parent_permlink: campaign.users[0].permlink };
+        operation = { author: faker.name.firstName(), parent_permlink: faker.random.string()};
         returnedValue = await campaignHelper.parseReservationConversation(operation);
       });
       it('parseReservationConversation should return true', async () => {
         expect(returnedValue).to.be.true;
-      });
-      it('should increment children to 1', async () => {
-        ({ result: { users: [user] } } = await CampaignModel
-          .findOne({ users: { $elemMatch: { permlink: operation.parent_permlink } } }));
-        expect(user.children).to.be.eq(1);
       });
     });
     describe('when function found campaign and user reserved it', async () => {
