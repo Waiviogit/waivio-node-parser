53ff57104b0a1c2852d4950da28540a170249c7a
olegtsigulov <olegcigulev@gmail.com>
2020-09-23 16:18:19 +0300

    add sentry

--- utilities/helpers/sentryHelper.js
+++ utilities/helpers/sentryHelper.js
@@ -4,7 +4,7 @@ const { telegramApi } = require('constants/appData');
 exports.sendSentryNotification = async () => {
   try {
     if (!['staging', 'production'].includes(process.env.NODE_ENV)) return;
-    const result = await axios.get(`${telegramApi.HOST}${telegramApi.BASE_URL}${telegramApi.SENTRY_ERROR}?app=notificationsApi`);
+    const result = await axios.get(`${telegramApi.HOST}${telegramApi.BASE_URL}${telegramApi.SENTRY_ERROR}?app=nodeParser`);
     return { result: result.data };
   } catch (error) {
     return { error };
