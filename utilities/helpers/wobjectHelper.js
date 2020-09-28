f31c200387dc7a8fc338a9894b50db7ab00f63c8
Boris Andrzhiievskyi <borees2@gmail.com>
2020-09-28 17:26:44 +0300

    fix

--- utilities/helpers/wobjectHelper.js
+++ utilities/helpers/wobjectHelper.js
@@ -69,7 +69,7 @@ const calculateApprovePercent = (field) => {
 
 const getWobjWinField = async ({ fieldName, authorPermlink }) => {
   const { app: { admins = [] } } = await App.getOne({ name: config.app });
-  const { wobjects: [{ fields } = {}] } = await Wobj.getSomeFields(
+  const { wobjects: [{ fields } = []] } = await Wobj.getSomeFields(
     fieldName, authorPermlink, true,
   );
 
