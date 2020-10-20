75f69c0debdeb57e3a4c15d4d5ece368158c2cb1
Boris Andrzhiievskyi <borees2@gmail.com>
2020-10-20 19:38:16 +0300

    handle error, calculate post.pending_payout_value, post.net_rshares

--- utilities/steemApi/usersUtil.js
+++ utilities/steemApi/usersUtil.js
@@ -18,9 +18,26 @@ const getUsers = async (accountNames) => {
     return { error };
   }
 };
+const parseToFloat = (balance) => parseFloat(balance.match(/.\d*.\d*/)[0]);
+
+const getCurrentPriceInfo = async () => {
+  const sbdMedian = await client.database.call('get_current_median_history_price', []);
+  const rewardFund = await client.database.call('get_reward_fund', ['post']);
+  const props = await client.database.getDynamicGlobalProperties();
+  return {
+    currentPrice: parseToFloat(sbdMedian.base) / parseToFloat(sbdMedian.quote),
+    rewardFund,
+    props,
+  };
+};
 
 const calculateVotePower = async ({ votesOps, posts }) => {
-  const { users } = await getUsers(_.map(votesOps, 'voter'));
+  const { users, error } = await getUsers(_.map(votesOps, 'voter'));
+  if (error) {
+    console.error(`${error.message}`);
+    return posts;
+  }
+  const { rewardFund } = await getCurrentPriceInfo();
 
   for (const vote of votesOps) {
     const account = _.find(users, (el) => el.name === vote.voter);
@@ -45,6 +62,16 @@ const calculateVotePower = async ({ votesOps, posts }) => {
       percent: vote.weight,
       rshares: Math.round(rShares),
     });
+    // net_rshares sum of all post active_votes rshares negative and positive
+    const tRShares = parseFloat(post.net_rshares) + rShares;
+    const s = parseFloat(rewardFund.content_constant);
+    const tClaims = (tRShares * (tRShares + (2 * s))) / (tRShares + (4 * s));
+
+    const rewards = parseFloat(rewardFund.reward_balance) / parseFloat(rewardFund.recent_claims);
+    const postValue = tClaims * rewards; // *price - to calculate in HBD
+
+    post.net_rshares = Math.round(tRShares);
+    post.pending_payout_value = postValue < 0 ? '0.000 HBD' : `${postValue.toFixed(3)} HBD`;
   }
   return posts;
 };
