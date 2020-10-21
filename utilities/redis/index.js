8c7f9f41af665173d427541adb6f8bca2f59bbd1
Boris Andrzhiievskyi <borees2@gmail.com>
2020-10-21 16:50:52 +0300

    redis add ttl

--- utilities/redis/index.js
+++ utilities/redis/index.js
@@ -4,4 +4,5 @@ module.exports = {
   redisGetter: require('./redisGetter'),
   redisSetter: require('./redisSetter'),
   unusedKeysCollector: require('./unusedKeysCollector'),
+  updatePriceInfo: require('./updatePriceInfo'),
 };
