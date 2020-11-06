150347f006859b714cea5f51160ea9431ec3b311
Boris Andrzhiievskyi <borees2@gmail.com>
2020-11-06 18:16:51 +0200

    fix comments

--- parsers/guestCommentParser.js
+++ parsers/guestCommentParser.js
@@ -1,21 +1,13 @@
 const { CommentModel } = require('models');
 const { guestHelpers } = require('utilities/guestOperations');
-const { postsUtil } = require('utilities/steemApi');
 
 exports.parse = async ({ operation, metadata }) => {
   const guestInfo = await guestHelpers.getFromMetadataGuestInfo({ operation, metadata });
   if (!guestInfo) return;
 
-  let { post: comment, err } = await postsUtil.getPost(operation.author, operation.permlink);
-  if (err || !comment) {
-    await new Promise((resolve) => setTimeout(resolve, 1500));
-    ({ post: comment, err } = await postsUtil.getPost(operation.author, operation.permlink));
+  Object.assign(operation, { root_author: 'monterey', root_permlink: 'test-post' });
 
-    if (err) return console.error(err || `Comment @${operation.author}/${operation.permlink} not found!`);
-  }
-
-  delete comment.active_votes;
-  const { error } = await CommentModel.createOrUpdate({ ...comment, guestInfo });
+  const { error } = await CommentModel.createOrUpdate({ ...operation, guestInfo });
   if (error) return console.error(error);
   console.log(`Guest comment created: ${operation.author}/${operation.permlink}, guest name: ${guestInfo.userId}`);
 };
