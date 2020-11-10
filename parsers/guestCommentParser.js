70106ceddd4c1b05d3afc50ab4ffa0fcfe354313
Boris Andrzhiievskyi <borees2@gmail.com>
2020-11-10 11:46:17 +0200

    fix pr

--- parsers/guestCommentParser.js
+++ parsers/guestCommentParser.js
@@ -1,13 +1,26 @@
 const { CommentModel } = require('models');
+const { postsUtil } = require('utilities/steemApi');
 const { guestHelpers } = require('utilities/guestOperations');
+const { setExpiredPostTTL } = require('utilities/redis/redisSetter');
 
 exports.parse = async ({ operation, metadata }) => {
   const guestInfo = await guestHelpers.getFromMetadataGuestInfo({ operation, metadata });
   if (!guestInfo) return;
 
-  Object.assign(operation, { root_author: 'monterey', root_permlink: 'test-post' });
-
-  const { error } = await CommentModel.createOrUpdate({ ...operation, guestInfo });
+  const { post: comment, err } = await postsUtil
+    .getPost(operation.parent_author, operation.parent_permlink);
+  if (err || !comment) {
+    return setExpiredPostTTL('notFoundGuestComment', `${operation.author}/${operation.permlink}`, 30);
+  }
+  const { error } = await CommentModel.createOrUpdate({
+    author: operation.author,
+    permlink: operation.permlink,
+    parent_author: comment.author,
+    parent_permlink: comment.permlink,
+    root_author: comment.parent_author,
+    root_permlink: comment.parent_permlink,
+    guestInfo,
+  });
   if (error) return console.error(error);
   console.log(`Guest comment created: ${operation.author}/${operation.permlink}, guest name: ${guestInfo.userId}`);
 };
