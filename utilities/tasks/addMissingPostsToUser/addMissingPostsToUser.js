e768bfe1532962aa98cfafe5a110240a1482f0db
Yaroslav Semeniuk <sem.yarik1@gmail.com>
2021-03-31 17:08:58 +0300

    GIT-2228 fixed error handling & process.argv in package.json

--- utilities/tasks/addMissingPostsToUser/addMissingPostsToUser.js
+++ utilities/tasks/addMissingPostsToUser/addMissingPostsToUser.js
@@ -6,25 +6,29 @@ const { sendSentryNotification } = require('utilities/helpers/sentryHelper');
 const UserModel = require('models/UserModel');
 const PostModel = require('models/PostModel');
 const { reblogPostParser } = require('parsers/userParsers');
-const { parseBodyWobjects } = require('../../helpers/postHelper');
+const { parseBodyWobjects } = require('utilities/helpers/postHelper');
 
 module.exports = async (name) => {
   const { user } = await UserModel.checkAndCreate(name);
-  user.wobjects_weight = 25000;
 
   const { posts, err } = await postsUtil.getPostsByAuthor(name);
-  if (err || !posts) {
+  if (err) {
     sendSentryNotification();
     Sentry.captureException(err);
   }
 
-  const reblogPosts = _.reject(posts, { author: name });
-  reblogPosts.forEach((post) => parseAndSavePost(post, post.author));
+  const reblogPosts = _.filter(posts, (post) => (post.author !== name));
+  for (const reblogPost of reblogPosts) {
+    await parseAndSavePost(reblogPost, reblogPost.author);
+  }
 
   for (const post of posts) {
     if (post.author !== name) {
-      const json = ['reblog', { account: name, author: post.author, permlink: post.permlink }];
-      await reblogPostParser({ json, account: { name }, fromTask: true });
+      await reblogPostParser({
+        json: ['reblog', { account: name, author: post.author, permlink: post.permlink }],
+        account: name,
+        fromTask: true,
+      });
     } else {
       await parseAndSavePost(post, user);
     }
@@ -35,18 +39,25 @@ const parseAndSavePost = async (post, user) => {
   let metadata;
   const { post: existPost } = await PostModel.findOne(post);
   if (existPost) return;
+
   post.author_weight = _.get(user, 'wobjects_weight', 0);
 
   try {
     metadata = JSON.parse(post.json_metadata);
   } catch (e) {
-    console.error(e);
+    sendSentryNotification();
+    Sentry.captureException(e);
   }
   if (!(await checkAppBlacklistValidity(metadata))) return;
+  if (metadata.app) {
+    post.app = metadata.app;
+  }
 
-  post.app = metadata.app;
   post.wobjects = await parseBodyWobjects(metadata, post.body);
 
   const { error } = await PostModel.create(post);
-  if (error) console.error(error);
+  if (error) {
+    sendSentryNotification();
+    Sentry.captureException(error);
+  }
 };
