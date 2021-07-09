dfe8a0084ad778f51f8d16d4edd18189d69c42d0
Yaroslav Semeniuk <sem.yarik1@gmail.com>
2021-07-09 14:35:31 +0300

    added a field for info from which user the reblogging is performed

--- parsers/userParsers.js
+++ parsers/userParsers.js
@@ -127,6 +127,7 @@ exports.reblogPostParser = async ({
     const data = {
       author: account, // person who make reblog
       permlink: `${_.get(json, '[1].author')}/${_.get(json, '[1].permlink')}`,
+      reblog_from: post.author,
       reblog_to: {
         author: _.get(json, '[1].author'), // author of source post
         permlink: _.get(json, '[1].permlink'), // permlink of source post
