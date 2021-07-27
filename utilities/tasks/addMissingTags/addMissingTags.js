8904850bf014b26127619bf184aa20c0f867193f
Boris Andrzhiievskyi <borees2@gmail.com>
2021-07-27 15:13:51 +0300

    addMissingTagsTask

---
+++ utilities/tasks/addMissingTags/addMissingTags.js
@@ -0,0 +1,34 @@
+const { createTags } = require('utilities/restaurantTagsParser/tagsParser');
+const { FIELDS_NAMES } = require('constants/wobjectsData');
+const sleep = require('util').promisify(setTimeout);
+const { Wobj } = require('models');
+const _ = require('lodash');
+
+module.exports = async (type) => {
+  const filter = {
+    object_type: type,
+    fields: {
+      $elemMatch: {
+        $or: [
+          { name: FIELDS_NAMES.NAME },
+          { name: FIELDS_NAMES.TITLE },
+          { name: FIELDS_NAMES.DESCRIPTION },
+        ],
+      },
+    },
+  };
+  const { result } = await Wobj.find(filter, { author_permlink: 1, fields: 1 });
+  for (const resultElement of result) {
+    const fields = _.filter(
+      resultElement.fields,
+      (el) => _.includes([FIELDS_NAMES.NAME, FIELDS_NAMES.TITLE, FIELDS_NAMES.DESCRIPTION], el.name),
+    );
+    for (const field of fields) {
+      const countAdded = await createTags({ field, authorPermlink: resultElement.author_permlink });
+      console.log('count added', countAdded);
+      await sleep(countAdded * 3000);
+      console.log('tick');
+    }
+  }
+  console.log('task completed');
+};
