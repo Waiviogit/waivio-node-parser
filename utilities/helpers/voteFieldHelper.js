da193d94c70c8b4a3f3a1065c96766e17ca3af01
Yaroslav Semeniuk <sem.yarik1@gmail.com>
2021-08-31 13:26:31 +0300

    added weight reduction for guest user

--- utilities/helpers/voteFieldHelper.js
+++ utilities/helpers/voteFieldHelper.js
@@ -58,6 +58,9 @@ const unVoteOnAppend = async (data) => {
 const addVoteOnField = async (data) => {
   data.percent = calculateVotePercent(data.percent);
   data.weight = (data.weight + data.rshares_weight * 0.25) * (data.percent / 100);
+  if (new RegExp('^waivio\_').test(data.voter)) {
+    data.weight = --data.weight;
+  }
   await upDownVoteOnAppend({ ...data });
 
   await Wobj.addVote({
