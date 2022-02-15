54ed791a44225a55508e46c99dcd5933cb31935f
unknown <Alishern@wizardsdev@gmail.com>
2022-02-15 13:53:46 +0600

    fix model

--- models/EngineAccountHistoryModel.js
+++ models/EngineAccountHistoryModel.js
@@ -4,18 +4,10 @@ const EngineAccountHistoryModel = require('database').models.EngineAccountHistor
 const create = async (data) => {
   const EngineAccountHistory = new EngineAccountHistoryModel(data);
   try {
-    return { engine_account_histories: await EngineAccountHistory.save() };
+    return { result: await EngineAccountHistory.save() };
   } catch (error) {
     return { error };
   }
 };
 
-const insertMany = async (docs) => {
-  try {
-    return { engine_account_histories: await EngineAccountHistoryModel.insertMany(docs) };
-  } catch (error) {
-    return { error };
-  }
-};
-
-module.exports = { create, insertMany };
+module.exports = { create };
