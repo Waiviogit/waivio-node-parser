7c57d4c0aa30c3ca29fe8d6eb48c0de8b89d40e4
MashaYakovenko <mariiay@wizardsdev.com>
2022-05-30 14:32:19 +0300

    task on updating db

--- utilities/tasks/updateObjectTypesExposedFields/updateObjectTypesExposedFields.js
+++ utilities/tasks/updateObjectTypesExposedFields/updateObjectTypesExposedFields.js
@@ -9,7 +9,6 @@ exports.updateObjectTypes = async () => {
   try {
     await ObjectType.updateMany({ name: { $in: OBJECT_TYPES_FOR_COMPANY } },
       { $addToSet: { exposedFields: FIELDS_NAMES.COMPANY_ID } });
-    await addSearchesFields(FIELDS_NAMES.COMPANY_ID);
   } catch (error) {
     console.log('Saving error', error);
     console.log('task completed');
