27c40effdc63b69d324e64c12b6f178aaa237784
MashaYakovenko <mariiay@wizardsdev.com>
2022-05-30 15:39:55 +0300

    fixed

--- utilities/tasks/updateObjectTypesExposedFields/updateObjectTypesExposedFields.js
+++ utilities/tasks/updateObjectTypesExposedFields/updateObjectTypesExposedFields.js
@@ -2,7 +2,7 @@ const { ObjectType } = require('database').models;
 
 exports.updateObjectTypes = async (updateObjectTypes, fieldName) => {
   try {
-    await ObjectType.updateMany({ name: { $in: updateObjectTypes } },
+    await ObjectType.updateMany({ name: { $in: updateObjectTypes.split(',') } },
       { $addToSet: { exposedFields: fieldName } });
     console.log('task completed');
   } catch (error) {
