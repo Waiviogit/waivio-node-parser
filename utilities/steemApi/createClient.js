2c31c5028b8b066451721a5aa30b5cc5d6f31f5a
fesmofet <borees2@gmail.com>
2022-06-02 18:56:18 +0300

    socket client fix

--- utilities/steemApi/createClient.js
+++ utilities/steemApi/createClient.js
@@ -1,6 +1,6 @@
 const { HIVE_MIND_NODES, HIVED_NODES } = require('constants/appData');
 const { Client } = require('@hiveio/dhive');
 
-exports.hiveMindClient = new Client(HIVE_MIND_NODES, { timeout: 8 * 1000, failoverThreshold: 4 });
+exports.hiveMindClient = new Client(HIVE_MIND_NODES, { timeout: 8 * 1000, failoverThreshold: 0 });
 
-exports.hivedClient = new Client(HIVED_NODES, { timeout: 8 * 1000, failoverThreshold: 4 });
+exports.hivedClient = new Client(HIVED_NODES, { timeout: 8 * 1000, failoverThreshold: 0 });
