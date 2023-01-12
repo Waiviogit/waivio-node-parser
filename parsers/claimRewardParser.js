baa5a5d5d011d66faf791f888b0464559e5568a8
fesmofet <borees2@gmail.com>
2023-01-12 14:31:23 +0200

    Git_4035

--- parsers/claimRewardParser.js
+++ parsers/claimRewardParser.js
@@ -1,7 +1,14 @@
 const notificationsUtil = require('utilities/notificationsApi/notificationsUtil');
+const redisSetter = require('utilities/redis/redisSetter');
+const { REDIS_KEYS } = require('constants/parsersData');
 
-const parse = async (operation) => {
+const parse = async (operation, transactionId) => {
   await notificationsUtil.custom(Object.assign(operation, { id: 'claimReward' }));
+
+  await redisSetter.publishToChannel({
+    channel: REDIS_KEYS.TX_ID_MAIN,
+    msg: transactionId,
+  });
 };
 
 module.exports = { parse };
