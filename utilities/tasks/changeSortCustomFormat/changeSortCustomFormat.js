743b1c4d727abd2118977981751337647b8c6001
fesmofet <borees2@gmail.com>
2023-02-09 17:14:23 +0200

    4940_task custom sort

---
+++ utilities/tasks/changeSortCustomFormat/changeSortCustomFormat.js
@@ -0,0 +1,52 @@
+const { FIELDS_NAMES } = require('constants/wobjectsData');
+const { WObject } = require('database').models;
+const jsonHelper = require('utilities/helpers/jsonHelper');
+const _ = require('lodash');
+
+const fieldsCanBeInSortCustom = [
+  FIELDS_NAMES.FORM,
+  FIELDS_NAMES.BUTTON,
+  FIELDS_NAMES.LIST_ITEM,
+  FIELDS_NAMES.NEWS_FILTER,
+  FIELDS_NAMES.BLOG,
+];
+
+module.exports = async () => {
+  try {
+    // const objects = await WObject.find({ author_permlink: '3a8vjn-campsite' }, { fields: 1 }).lean();
+    const objects = await WObject.find(
+      { 'fields.name': FIELDS_NAMES.SORT_CUSTOM },
+      { fields: 1, author_permlink: 1 },
+    ).lean();
+
+    for (const object of objects) {
+      for (const field of object.fields) {
+        if (field.name !== FIELDS_NAMES.SORT_CUSTOM) continue;
+        const oldSort = jsonHelper.parseJson(field.body, null);
+        if (!oldSort) continue;
+        const filterFunction = (f) => f._id < field._id
+          && _.includes(fieldsCanBeInSortCustom, f.name)
+          && !_.includes(
+            oldSort,
+            f.name === FIELDS_NAMES.LIST_ITEM ? f.body : f.permlink,
+          );
+        const filter = _.chain(object.fields)
+          .filter(filterFunction)
+          .map((f) => (f.name === FIELDS_NAMES.LIST_ITEM ? f.body : f.permlink))
+          .value();
+
+        if (!_.isEmpty(filter)) {
+          console.log();
+        }
+
+        const newBody = JSON.stringify({
+          sort: oldSort,
+          filter,
+        });
+        console.log();
+      }
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+};
