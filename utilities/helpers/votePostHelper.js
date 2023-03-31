76a1cb07ea1d9c1b21691755594f9a3d51377da9
fesmofet <borees2@gmail.com>
2023-03-31 10:03:57 +0300

    test_no_validator

--- utilities/helpers/votePostHelper.js
+++ utilities/helpers/votePostHelper.js
@@ -21,7 +21,6 @@ const voteOnPost = async (data) => {
       await upVoteOnPost(data, weight); // case for up-vote
     }
   }
-  // await updateVotesOnPost(data);
 };
 
 // method also using as undo previous vote before up- or down-vote
