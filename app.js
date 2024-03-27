91ff5ef590c12881709b459f16de775a412abe66
fesmofet <borees2@gmail.com>
2024-03-27 12:42:12 +0200

    6522_guest_signature remove router and morgan

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const logger = require('morgan');
-const { routes } = require('routes');
 const Sentry = require('@sentry/node');
 const Tracing = require('@sentry/tracing');
 const { sendSentryNotification } = require('utilities/helpers/sentryHelper');
@@ -14,26 +12,14 @@ Sentry.init({
   environment: process.env.NODE_ENV,
   dsn: process.env.SENTRY_DNS,
   integrations: [
-    // enable HTTP calls tracing
-    new Sentry.Integrations.Http({ tracing: true }),
     // enable Express.js middleware tracing
     new Tracing.Integrations.Express({ app }),
   ],
 });
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use('/', routes);
 
 process.on('unhandledRejection', (error) => {
   sendSentryNotification();
   Sentry.captureException(error);
-});
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
 });
 
 runStream().catch((err) => {
