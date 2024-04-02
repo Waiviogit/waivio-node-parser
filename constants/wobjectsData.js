00ec2907a3cf34072fb6400f5965eb6263914c7e
fesmofet <borees2@gmail.com>
2024-04-02 15:51:39 +0300

    6565_admin_assigned

--- constants/wobjectsData.js
+++ constants/wobjectsData.js
@@ -69,6 +69,7 @@ exports.FIELDS_NAMES = {
   MAP_MOBILE_VIEW: 'mapMobileView',
   MAP_DESKTOP_VIEW: 'mapDesktopView ',
   WALLET_ADDRESS: 'walletAddress',
+  ADMIN_ASSIGNED: 'adminAssigned',
 };
 
 exports.SEARCH_FIELDS = [
